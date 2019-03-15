24aeadcd5aacf0a4ce98c45c19b38b904d150e4c
alexmorgan1995 <48563535+alexmorgan1995@users.noreply.github.com>
2019-03-15 13:47:51 +0000

    Add files via upload

---
+++ WM - Complex Model - Resistant and Susceptible Infections - R v10 - BASIC.R
@@ -0,0 +1,308 @@
+setwd("//csce.datastore.ed.ac.uk/csce/biology/users/s1678248/PhD/Mathematical Models")
+
+rm(list=ls())
+
+library("deSolve")
+library("fast")
+library("sensitivity")
+library("ggplot2")
+library("plotly")
+library("tidyr")
+
+#### Model Functions + Output ####
+amr <- function(time, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    dSa = ua + ra*(Ia + Ira) + tau*Ia - (betaAA*Ia*Sa) - (betaAH*Ih*Sa) - (betaAH*Irh*Sa) - (betaAA*Ira*Sa) - ua*Sa  
+    dIa = betaAA*Ia*Sa + betaAH*Ih*Sa + phi*Ira - tau*Ia - tau*theta*Ia - ra*Ia - ua*Ia
+    dIra = betaAH*Irh*Sa + betaAA*Ira*Sa + tau*theta*Ia - phi*Ira - ra*Ira - ua*Ira
+    
+    dSh = uh + rh*(Ih+Irh) - (betaHH*Ih*Sh) - (betaHH*Irh*Sh) - (betaHA*Ia*Sh) - (betaHA*Ira*Sh) - uh*Sh 
+    dIh = betaHH*Ih*Sh + betaHA*Ia*Sh - rh*Ih - uh*Ih 
+    dIrh = betaHH*Irh*Sh + betaHA*Ira*Sh - rh*Irh - uh*Irh 
+    return(list(c(dSa,dIa,dIra,dSh,dIh,dIrh)))
+  })
+}
+
+parms = fast_parameters(minimum = c(0,0,0,0,0,0,0,0,0,0,0), maximum = c(1,1,1,1,1,1,1,1,1,1,1), 
+                        factor=11, names = c("ra", "rh" ,"ua", "uh", "betaAA", "betaAH", "betaHH", "betaHA",
+                                             "phi", "tau", "theta"))
+
+init <- c(Sa=0.99, Ia=0.01, Ira=0, Sh=1, Ih=0, Irh=0)
+
+times <- c(0,9999,10000) #Specified the time intervals to be massive as dynamics do not matter
+output <- data.frame()
+for (i in 1:nrow(parms)) {
+  temp <- data.frame(matrix(NA, nrow = 1, ncol=4))
+  parms1 = c(ra = parms$ra[i], rh = parms$rh[i] , ua = parms$ua[i], uh = parms$uh[i], betaAA = parms$betaAA[i],
+             betaAH = parms$betaAH[i], betaHH = parms$betaHH[i], betaHA = parms$betaHA[i], phi=parms$phi[i],
+             tau=parms$tau[i], theta=parms$theta[i])
+  out <- ode(y = init, func = amr, times = times, parms = parms1)
+  print(out[nrow(out),7])
+  temp[1,1] <- as.numeric(out[nrow(out),5])
+  temp[1,2] <- as.numeric(out[nrow(out),6])
+  temp[1,3] <- as.numeric(out[nrow(out),7])
+  temp[1,4] <- as.numeric(out[nrow(out),7]) + as.numeric(out[nrow(out),6])
+  if(as.numeric(out[nrow(out),6]) !=0 & (as.numeric(out[nrow(out),6])/ as.numeric(out[nrow(out)-1,6]))>0.99999999999 & 
+     (as.numeric(out[nrow(out),6])/ as.numeric(out[nrow(out)-1,6]))<1.00000000001){
+    temp[1,5] <- "Yes"
+  } 
+  else {
+    temp[1,5] <- "No - Changing"
+  }
+  if (as.numeric(out[nrow(out),5])>0.999999999 & as.numeric(out[nrow(out),5])< 1.000000001){
+    temp[1,5] <- "No"
+  }
+  output <- rbind.data.frame(output, temp)
+}
+
+colnames(output)[1:5] <- c("SuscHumans","InfHumans","ResInfHumans","IComb","StdyEquilibrium")
+
+plot(output$InfHumans, output$ResInfHumans, xlim = c(-0.001,1))
+
+#### SENSITIVITY ANALYSIS #####
+sensit <- output$IComb
+sens<-sensitivity(x=sensit, numberf=11, make.plot=T, names = c("ra", "rh" ,"ua", "uh", "betaAA", "betaAH", "betaHH", "betaHA",
+                                                               "phi", "tau", "theta"))
+
+df.equilibrium <- data.frame(parameter=rbind("ra", "rh" ,"ua", "uh", "betaAA", "betaAH", "betaHH", "betaHA",
+                                             "phi","tau", "theta"), value=sens)
+
+p <- ggplot(df.equilibrium, aes(parameter, value))
+p + geom_bar(stat="identity", fill="grey23")
+
+#### Model Testbed#####
+#For Particular Parameter Combinations#
+
+amr <- function(time, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    dSa = ua + ra*(Ia + Ira) + tau*Ia - (betaAA*Ia*Sa) - (betaAH*Ih*Sa) - (betaAH*Irh*Sa) - (betaAA*Ira*Sa) - ua*Sa  
+    dIa = betaAA*Ia*Sa + betaAH*Ih*Sa + phi*Ira - tau*Ia - tau*theta*Ia - ra*Ia - ua*Ia
+    dIra = betaAH*Irh*Sa + betaAA*Ira*Sa + tau*theta*Ia - phi*Ira - ra*Ira - ua*Ira
+    
+    dSh = uh + rh*(Ih+Irh) - (betaHH*Ih*Sh) - (betaHH*Irh*Sh) - (betaHA*Ia*Sh) - (betaHA*Ira*Sh) - uh*Sh 
+    dIh = betaHH*Ih*Sh + betaHA*Ia*Sh - rh*Ih - uh*Ih 
+    dIrh = betaHH*Irh*Sh + betaHA*Ira*Sh - rh*Irh - uh*Irh 
+    return(list(c(dSa,dIa,dIra,dSh,dIh,dIrh)))
+  })
+}
+
+init <- c(Sa=0.99, Ia=0.01, Ira=0, Sh=0.99, Ih=0.01, Irh=0)
+times1 <- seq(0,1000,by=10)
+
+parms4 = c(ra = 0.01, rh =  0.5, ua = 0.01, uh = 0.001, betaAA = 0.05, betaAH = 0.01, betaHH = 0.01, 
+           betaHA = 0.01, phi = 0.95, tau = 0.05, theta = 0.5)
+
+out <- ode(y = init, func = amr, times = times1, parms = parms4)
+head(out,100) #Provides the first 10 timesteps for the model output
+
+par(mfrow=c(1,2))
+plot(out[,"time"],out[,"Sa"], xlab = "Time (Days)",ylab="Proportion (Animals)", type="l", lwd=2, col="green", ylim = c(-0.01, 1))
+lines(out[,"time"],out[,"Ia"], type="l", lwd=2, col="red")
+lines(out[,"time"],out[,"Ira"],type="l", lwd=2, col="blue")
+legend(x=500, y=0.97, legend= c("Susceptible", "Infected (S)", "Infected (R)"),col=c("green","red","blue"),lty=1,cex=0.9)
+
+plot(out[,"time"],out[,"Sh"], xlab = "Time (Days)",ylab="Proportion(Humans)", type="l", lwd=2, col="green", ylim = c(-0.01, 0.1))
+lines(out[,"time"],out[,"Ih"], type="l", lwd=2, col="red")
+lines(out[,"time"],out[,"Irh"], type="l", lwd=2, col="blue")
+legend(x=500, y=0.045, legend= c("Susceptible", "Infected (S)", "Infected (R)"),col=c("green","red","blue"),lty=1,cex=0.9)
+
+#### Comb IRH and IH Measure Testing - Effect of Treatment and Surface Plots + Theta and Other Parameters ####
+
+# Testing for the Effect of Treatment on a Combined Measure
+parmtau <- seq(0,1,by=0.01)
+init <- c(Sa=0.99, Ia=0.01, Ira=0, Sh=0.99, Ih=0.01, Irh=0)
+output1 <- data.frame()
+times <- c(0,9999,10000)
+
+
+for (i in 1:length(parmtau)) {
+  temp <- data.frame(matrix(NA, nrow = 1, ncol=5))
+  parms2 = c(ra = 0.01, rh =  0.01, ua = 0.01, uh = 0.001, betaAA = 0.05, betaAH = 0.001, betaHH = 0.001, 
+             betaHA = 0.001, phi = 0.05, kappa = 0.05, nu = 0.05, tau = parmtau[i], theta = .8)
+  out <- ode(y = init, func = amr, times = times, parms = parms2)
+  temp[1,1] <- i/100
+  temp[1,2] <- as.numeric(out[nrow(out),3])
+  temp[1,3] <- as.numeric(out[nrow(out),4])
+  temp[1,4] <- out[nrow(out),3] + out[nrow(out),4]
+  temp[1,5] <- as.numeric(out[nrow(out),6])
+  temp[1,6] <- as.numeric(out[nrow(out),7])
+  temp[1,7] <- out[nrow(out),6] + out[nrow(out),7]
+  output1 <- rbind.data.frame(output1, temp)
+}
+
+plot(output1$X1, output1$X4, type="l", lwd=2, col="red", xlim = c(0,1), ylim = c(0,1), ylab="Prevalence of Infected Fraction",
+     xlab = "Fraction of Animal Population Antibiotics are Used")
+legend(x=0.2, y=1, legend= c("Combined IRH* and IH*"),col=c("red"),lty=1,cex=0.9)
+plot(output1$X1, output1$V7, type="l", lwd=2, col="red", xlim = c(0,1), ylim = c(0,1), ylab="Prevalence of Infected Fraction",
+     xlab = "Fraction of Animal Population Antibiotics are Used")
+legend(x=0.2, y=1, legend= c("Combined IRH* and IH*"),col=c("red"),lty=1,cex=0.9)
+
+#### Testbed Parameter Space Testing ####
+
+#thetarange <- seq(0,1, by=0.1)
+taurange <- seq(0,1, by=0.01)
+thetarange <- seq(0,1, by=0.01)
+betaHArange <- seq(0,1, by=0.01)
+
+combparm1 <- expand.grid(taurange, betaHArange)
+colnames(combparm1)[1:2] <- c("tau","betaHA")
+
+init <- c(Sa=0.99, Ia=0.01, Ira=0, Sh=1, Ih=0, Irh=0)
+times <- c(0,9999,10000)
+surfaceoutput1 <- data.frame()
+
+for (i in 1:nrow(combparm1)) {
+  temp <- data.frame(matrix(NA, nrow = 1, ncol=5))
+  parms1 = c(ra = 0.01, rh =  0.01, ua = 0.01, uh = 0.001, betaAA = 0.05, betaAH = 0.001, betaHH = 0.001, 
+             betaHA = combparm1[i,2], phi = 0.05, tau = combparm1[i,1], theta = 0.2)
+  out <- ode(y = init, func = amr, times = times, parms = parms1)
+  print(out[nrow(out),7])
+  temp[1,1] <- combparm1[i,1]
+  temp[1,2] <- combparm1[i,2]
+  temp[1,3] <- out[nrow(out),5]
+  temp[1,4] <- out[nrow(out),6]
+  temp[1,5] <- out[nrow(out),7]
+  temp[1,6] <- out[nrow(out),6] + out[nrow(out),7]
+  print(temp[1,6])
+  surfaceoutput1 <- rbind.data.frame(surfaceoutput1, temp)
+}
+
+colnames(surfaceoutput1)[1:6] <- c("tau","betaHA","SuscHum","InfSensHum", "InfResHum", "Comb Inf Res/Sens Humans")
+plot(surfaceoutput1$InfSensHum, surfaceoutput1$InfResHum, xlim = c(-0.001,1), ylim = c(-0.001,1))
+
+surfaceoutputplot <- surfaceoutput1[,c(1,2,6)]
+
+#surfaceoutputplot$new <- round(surfaceoutputplot$`Comb Inf Res/Sens Humans`, digits = 6)
+surfaceoutputplot$new <- NULL
+
+mat <- spread(surfaceoutputplot, key = "betaHA", value = "Comb Inf Res/Sens Humans")
+row.names(mat) <- mat$tau
+mat$tau <- NULL
+mat1 <- data.matrix(mat)
+
+p5 <- plot_ly(z = mat1, x = taurange, y = betaHArange) %>% add_surface(
+  cmin = 0, cmax = 1
+  ) %>% layout(
+    title = "Comb Equilibrium Prevalence of I<sub>H</sub>*",
+    scene = list(
+      camera = list(eye = list(x = -1.25, y = 1.25, z = 0.5)),
+      xaxis = list(title = "betaHA", nticks = 8, range = c(0,1)),
+      yaxis = list(title = "tau", nticks = 8, range = c(0,1)),
+      zaxis = list(title = 'IH* + IRH*', nticks = 8, range = c(-0.00001,1)),
+      aspectratio=list(x=0.8,y=0.8,z=0.8)))
+p5 
+
+p6 <- plot_ly(x = taurange, y = betaHArange, z = mat1, type = "contour", transpose = TRUE,
+              contours = list(start = -0.0001, end = 1, size = 0.05),
+              colorbar = list(title = "I<sub>RH</sub>* + I<sub>H</sub>*")) %>% 
+  layout(title = "Comb Equilibrium Prevalence of I<sub>H</sub>*",
+         xaxis = list(title = "Tau"),
+         yaxis = list(title = "BetaHA"))
+p6
+
+#### Change in Icomb* ####
+betaHArange <- seq(0,1, by=0.001)
+betaAArange <- seq(0,1, by=0.001)
+tauchange <- data.frame()
+
+for (i in 1:length(betaHArange)) {
+  temp <- data.frame(matrix(NA, nrow = 1, ncol=4))
+  parms1 = c(ra = 0.01, rh =  0.01, ua = 0.01, uh = 0.001, betaAA = betaAArange[i], betaAH = 0.001, betaHH = 0.001, 
+             betaHA = 0.001,  phi = 0.05, kappa = 0.05, nu = 0.05, tau = 0, theta = 0.2)
+  parms2 = c(ra = 0.01, rh =  0.01, ua = 0.01, uh = 0.001, betaAA = betaAArange[i], betaAH = 0.001, betaHH = 0.001, 
+             betaHA = 0.001, phi = 0.05, kappa = 0.05, nu = 0.05, tau = 1, theta = 0.2)
+  out <- ode(y = init, func = amr, times = times, parms = parms1)
+  out1 <- ode(y = init, func = amr, times = times, parms = parms2)
+  temp[1,1] <- betaHArange[i]
+  temp[1,2] <- round(out[nrow(out),6],digits = 5) + round(out[nrow(out),7], digits = 5)
+  temp[1,3] <- round(out1[nrow(out),6], digits = 5) + round(out1[nrow(out),7], digits = 5)
+  temp[1,4] <- (((temp[1,2])/(temp[1,3])) - 1)*100
+  print(temp[1,4])
+  tauchange <- rbind.data.frame(tauchange, temp)
+}
+
+for (i in 1:length(betaHArange)) {
+  temp <- data.frame(matrix(NA, nrow = 1, ncol=4))
+  parms1 = c(ra = 0.01, rh =  0.01, ua = 0.01, uh = 0.001, betaAA = 0.01, betaAH = 0.001, betaHH = 0.001, 
+             betaHA = betaHArange[i], phi = 0.05, kappa = 0.05, nu = 0.05, tau = 0, theta = 0.2)
+  parms2 = c(ra = 0.01, rh =  0.01, ua = 0.01, uh = 0.001, betaAA = 0.01, betaAH = 0.001, betaHH = 0.001, 
+             betaHA = betaHArange[i], phi = 0.005, kappa = 0.005, nu = 0.005, tau = 1, theta = 0.2)
+  out <- ode(y = init, func = amr, times = times, parms = parms1)
+  out1 <- ode(y = init, func = amr, times = times, parms = parms2)
+  temp[1,1] <- betaHArange[i]
+  temp[1,2] <- round(out[nrow(out),6],digits = 6) + round(out[nrow(out),7], digits = 6)
+  temp[1,3] <- round(out1[nrow(out),6], digits = 6) + round(out1[nrow(out),7], digits = 6)
+  temp[1,4] <- (((temp[1,2])/(temp[1,3])) - 1)*100
+  print(temp[1,4])
+  tauchange <- rbind.data.frame(tauchange, temp)
+}
+
+surfaceoutputplot$new <- round(surfaceoutputplot$`Comb Inf Res/Sens Humans`, digits = 5)
+
+colnames(tauchange)[1:4] <- c("BetaHA","ICombTau0","ICombTau1","ChangeIComb")
+plot(tauchange$BetaHA, tauchange$ChangeIComb, type = "l", xlab = "BetaHA",
+     ylab = "% Increase in IComb* - Antibiotic Curtailment", lwd=2)
+
+#### ####
+
+betaAArange <- seq(0,1, by=0.01)
+betaHArange <- seq(0,1, by=0.01)
+
+tauchange <- data.frame()
+
+combparmtau <- expand.grid(betaAArange, betaHArange)
+colnames(combparmtau)[1:2] <- c("betaAArange","betaHArange")
+
+
+init <- c(Sa=0.99, Ia=0.01, Ira=0, Sh=1, Ih=0, Irh=0)
+times <- c(0,9999,10000) 
+
+for (i in 1:nrow(combparmtau)) {
+  temp <- data.frame(matrix(NA, nrow = 1, ncol=5))
+  parms1 = c(ra = 0.01, rh =  0.01, ua = 0.01, uh = 0.001, betaAA = combparmtau[i,1], betaAH = 0.001, betaHH = 0.001, 
+             betaHA = combparmtau[i,2], phi = 0.05, kappa = 0.05, nu = 0.05, tau = 0, theta = 0.2)
+  parms2 = c(ra = 0.01, rh =  0.01, ua = 0.01, uh = 0.001, betaAA = combparmtau[i,1], betaAH = 0.001, betaHH = 0.001, 
+             betaHA = combparmtau[i,2], phi = 0.05, kappa = 0.05, nu = 0.05, tau = 1, theta = 0.2)
+  out <- ode(y = init, func = amr, times = times, parms = parms1)
+  out1 <- ode(y = init, func = amr, times = times, parms = parms2)
+  temp[1,1] <- combparmtau[i,1]
+  temp[1,2] <- combparmtau[i,2]
+  temp[1,3] <- out[nrow(out),6] + out[nrow(out),7]
+  temp[1,4] <- out1[nrow(out),6] + out1[nrow(out),7]
+  temp[1,5] <- (out[nrow(out),6] + out[nrow(out),7]) - (out1[nrow(out),6] + out1[nrow(out),7])
+  print(temp[1,5])
+  print(i)
+  tauchange <- rbind.data.frame(tauchange, temp)
+}
+
+colnames(tauchange)[1:5] <- c("BetaAA","BetaHA","ICombTau0","ICombTau1","ChangeIComb")
+plot(tauchange$BetaHA, tauchange$ChangeIComb, type = "l", xlab = "BetaHA",
+     ylab = "CHange in IComb (Tau = 1 -> Tau = 0)")
+
+
+tauchangeplot <- tauchange[,c(1,2,5)]
+
+mat <- spread(tauchangeplot, key = "BetaAA", value = "ChangeIComb")
+row.names(mat) <- mat$BetaHA
+mat$BetaHA <- NULL
+mat1 <- data.matrix(mat)
+
+p5 <- plot_ly(z = mat1, x = betaHArange, y = betaAArange) %>% add_surface(
+  cmin = 0, cmax = 1
+) %>% layout(
+  title = "Comb Equilibrium Prevalence of I<sub>H</sub>*",
+  scene = list(
+    camera = list(eye = list(x = -1.25, y = 1.25, z = 0.5)),
+    xaxis = list(title = "betaAArange", nticks = 8, range = c(0,1)),
+    yaxis = list(title = "betaHArange", nticks = 8, range = c(0,1)),
+    zaxis = list(title = 'Chnage in IComb*', nticks = 8, range = c(-0.00001,1)),
+    aspectratio=list(x=0.8,y=0.8,z=0.8)))
+p5 
+
+p6 <- plot_ly(x = betaAArange, y = betaHArange, z = mat1, type = "contour", transpose = TRUE,
+              contours = list(start = -0.0001, end = 1, size = 0.05),
+              colorbar = list(title = "I<sub>RH</sub>* + I<sub>H</sub>*")) %>% 
+  layout(title = "Comb Equilibrium Prevalence of I<sub>H</sub>*",
+         xaxis = list(title = "BetaAA"),
+         yaxis = list(title = "BetaHA"))
+p6
