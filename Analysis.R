92f97d3d9180d1273c48298ff5333eb875a666f1
alexmorgan1995 <a.morgan@outlook.com>
2020-06-16 00:37:01 +0100

    Update Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -4,7 +4,6 @@ library("bayestestR"); library("tmvtnorm"); library("ggpubr")
 rm(list=ls())
 setwd("C:/Users/amorg/Documents/PhD/Chapter_2/Chapter2_Fit_Data/FinalData")
 
-
 # Model Functions ----------------------------------------------------------
 
 #Function to remove negative prevalence values and round large DP numbers
@@ -41,73 +40,97 @@ import <- function(id) {
 # Data Import -------------------------------------------------------------
 #Import of Posterior Distributions
 data <- do.call(rbind, list(import(tet), import(amp), import(broil)))
+colnames(data)[3:4] <- c("betaAA", "alpha")
 
 #Import of Fitting Data
 dataamp <- read.csv("resistanceprofAnim_amp.csv")
 dataamp$lower <- unlist(lapply(1:nrow(dataamp), function(i) prop.test(dataamp$Positive.Sample[i],dataamp$N[i])[[6]][[1]]))
 dataamp$upper <- unlist(lapply(1:nrow(dataamp), function(i) prop.test(dataamp$Positive.Sample[i],dataamp$N[i])[[6]][[2]]))
+dataamp <- dataamp[!dataamp$N < 10,]
 
 datatetra <- read.csv("resistanceprofAnim_v1.csv")
 datatetra$lower <- unlist(lapply(1:nrow(datatetra), function(i) prop.test(datatetra$Positive.Sample[i],datatetra$N[i])[[6]][[1]]))
 datatetra$upper <- unlist(lapply(1:nrow(datatetra), function(i) prop.test(datatetra$Positive.Sample[i],datatetra$N[i])[[6]][[2]]))
+datatetra <- datatetra[!datatetra$N < 10,]
 
 databroil <- read.csv("salm_broilers_2018.csv")
 databroil$lower <- unlist(lapply(1:nrow(databroil), function(i) prop.test(databroil$Positive.Sample[i],databroil$N[i])[[6]][[1]]))
 databroil$upper <- unlist(lapply(1:nrow(databroil), function(i) prop.test(databroil$Positive.Sample[i],databroil$N[i])[[6]][[2]]))
+databroil <- databroil[!databroil$N < 10,]
 
 #Obtain the MAPs for each dataset
 
 MAPtet <- c("phi" <- map_estimate(data$phi[which(data$group == "data5" & data$fit == "tet")]),
             "theta" <- map_estimate(data$theta[which(data$group == "data5" & data$fit == "tet")]),
-            "betaAA" <- map_estimate(data$d_betaAA[which(data$group == "data5" & data$fit == "tet")]),
-            "alpha" <- map_estimate(data$d_alpha[which(data$group == "data5" & data$fit == "tet")]))
+            "betaAA" <- map_estimate(data$betaAA[which(data$group == "data5" & data$fit == "tet")]),
+            "alpha" <- map_estimate(data$alpha[which(data$group == "data5" & data$fit == "tet")]))
 
 MAPamp <- c("phi" <- map_estimate(data$phi[which(data$group == "data5" & data$fit == "amp")]),
             "theta" <- map_estimate(data$theta[which(data$group == "data5" & data$fit == "amp")]),
-            "betaAA" <- map_estimate(data$d_betaAA[which(data$group == "data5" & data$fit == "amp")]),
-            "alpha" <- map_estimate(data$d_alpha[which(data$group == "data5" & data$fit == "amp")]))
+            "betaAA" <- map_estimate(data$betaAA[which(data$group == "data5" & data$fit == "amp")]),
+            "alpha" <- map_estimate(data$alpha[which(data$group == "data5" & data$fit == "amp")]))
 
 MAPbroil <- c("phi" <- map_estimate(data$phi[which(data$group == "data5" & data$fit == "broil")]),
             "theta" <- map_estimate(data$theta[which(data$group == "data5" & data$fit == "broil")]),
-            "betaAA" <- map_estimate(data$d_betaAA[which(data$group == "data5" & data$fit == "broil")]),
-            "alpha" <- map_estimate(data$d_alpha[which(data$group == "data5" & data$fit == "broil")]))
+            "betaAA" <- map_estimate(data$betaAA[which(data$group == "data5" & data$fit == "broil")]),
+            "alpha" <- map_estimate(data$alpha[which(data$group == "data5" & data$fit == "broil")]))
 
 MAP <- rbind(MAPtet, MAPamp, MAPbroil); colnames(MAP) <- c("phi", "theta", "betaAA", "alpha")
 
 # ABC-SMC Posterior -------------------------------------------------------
 
-testphi <- melt(rbind(data1, data2, data3, data4, data5), id.vars = "group",measure.vars = "phi")
-testtheta <- melt(rbind(data1, data2, data3, data4,data5), id.vars = "group",measure.vars = "theta")
-testbetaAA <- melt(rbind(data1, data2, data3, data4, data5), id.vars = "group",measure.vars = "d_betaAA")
-testalpha <- melt(rbind(data1, data2, data3, data4, data5), id.vars = "group",measure.vars = "d_alpha")
+mybiglist1 <- list()
 
-p1 <- ggplot(testphi, aes(x=value, fill=group)) + geom_density(alpha=.5) + 
-  scale_x_continuous(expand = c(0, 0), name = expression(paste("Rate of Antibiotic-Resistant to Antibiotic-Sensitive Reversion (", phi, ")"))) + 
-  scale_y_continuous(limits = c(0,350), expand = c(0, 0), name = "") +
-  labs(fill = NULL) + scale_fill_discrete(labels = c("Generation 1", "Generation 2", "Generation 3", "Generation 4", "Generation 5"))+
-  theme(legend.text=element_text(size=14),  axis.text=element_text(size=14),
-        axis.title.y=element_text(size=14),axis.title.x= element_text(size=14), plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
+for(j in 1:length(unique(data$fit))) {
+  mybiglist2 <- list()
+  
+  for (i in 1:length(colnames(MAP))) { # Loop over loop.vector
+    
+    dens <- density(data[,colnames(MAP)[i]][data$fit == unique(data$fit)[j] & data$group == "data5"])
+    dataplot <- melt(data[data$fit == unique(data$fit)[j],], id.vars = "group", measure.vars = colnames(MAP)[i])
+    
+    mybiglist2[[i]] <- local({
+      i = i
+      p1 <- ggplot(data[data$fit == unique(data$fit)[j],], aes(x=get(colnames(MAP)[i]), fill=group)) + geom_density(alpha=.5) + theme_bw() + 
+        scale_y_continuous(limits = c(0,(max(dens$y)*1.1)), expand = c(0, 0), name = " ") +
+        labs(fill = NULL) + scale_fill_discrete(labels = c("Generation 1", "Generation 2", "Generation 3", "Generation 4", "Generation 5"))+
+        theme(legend.text=element_text(size=10), axis.text.x=element_text(size=10),axis.ticks.y=element_blank(), axis.text.y=element_blank(),
+              axis.title.y=element_text(size=10), axis.title.x= element_text(size=10), plot.margin = unit(c(0.15,0.4,0.15,0.55), "cm"))
+      if(colnames(MAP)[i] == "phi") {
+        p1 <- p1 + scale_x_continuous(limits = c(0,0.03), expand = c(0, 0), name = expression(paste("Rate of Resistance Reversion (", phi, ")"))) 
+      }
+      if(colnames(MAP)[i] == "theta") {
+        p1 <- p1 + scale_x_continuous(limits = c(0,0.1),expand = c(0, 0), name = expression(paste("Efficacy of Antibiotic-Mediated Recovery (", theta, ")"))) 
+      }
+      if(colnames(MAP)[i] == "betaAA") {
+        p1 <- p1 + scale_x_continuous(limits = c(0,0.3),expand = c(0, 0), name = expression(paste("Rate of Animal-to-Animal Transmission (", beta[AA], ")"))) 
+      }
+      if(colnames(MAP)[i] == "alpha") {
+        p1 <- p1 + scale_x_continuous(limits = c(0,0.6),expand = c(0, 0), name = expression(paste("Antibiotic-Resistant Fitness Cost (", alpha, ")"))) 
+      }
+      return(p1)
+      
+    })
+    print(paste0("Plot Parameter: ",colnames(MAP)[i], " | Data: ", unique(data$fit)[j] ))
+  }
+  mybiglist1[[unique(data$fit)[j]]] <- mybiglist2
+}
 
-p2 <- ggplot(testtheta, aes(x=value, fill=group)) + geom_density(alpha=.5)+
-  scale_x_continuous(expand = c(0, 0), name = expression(paste("Efficacy of Antibiotic-Mediated Animal Recovery (", theta, ")"))) + 
-  scale_y_continuous(limits = c(0,45), expand = c(0, 0), name = "") +
-  labs(fill = NULL) + scale_fill_discrete(labels = c("Generation 1", "Generation 2", "Generation 3", "Generation 4", "Generation 5")) +
-  theme(legend.text=element_text(size=14),  axis.text=element_text(size=14),
-        axis.title.y=element_text(size=14),axis.title.x= element_text(size=14), plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
+abc <- ggarrange(mybiglist1[[1]][[1]],mybiglist1[[2]][[1]],mybiglist1[[3]][[1]],
+                 mybiglist1[[1]][[2]],mybiglist1[[2]][[2]],mybiglist1[[3]][[2]],
+                 mybiglist1[[1]][[3]],mybiglist1[[2]][[3]],mybiglist1[[3]][[3]],
+                 mybiglist1[[1]][[4]],mybiglist1[[2]][[4]],mybiglist1[[3]][[4]],
+                 nrow = 4, ncol =3, 
+                 labels =  c("A","B","C",
+                             "","","",
+                             "","","",
+                             "","",""),
+                 font.label = c(size = 20), common.legend = TRUE, legend = "bottom",
+                 align = "hv")
 
-p3<- ggplot(testbetaAA, aes(x=value, fill=group)) + geom_density(alpha=.5)+
-  scale_x_continuous(expand = c(0, 0), name = expression(paste("Rate of Animal-to-Animal Transmission (", beta[AA], ")"))) + 
-  scale_y_continuous(limits = c(0,40),expand = c(0, 0), name = "") +
-  labs(fill = NULL) + scale_fill_discrete(labels = c("Generation 1", "Generation 2", "Generation 3", "Generation 4", "Generation 5")) +
-  theme(legend.text=element_text(size=14),  axis.text=element_text(size=14),
-        axis.title.y=element_text(size=14),axis.title.x= element_text(size=14), plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
+ggsave(abc, filename = "ABC_SMC_Post.png", dpi = 300, type = "cairo", width = 10, height = 15, units = "in",
+       path = "C:/Users/amorg/Documents/PhD/Chapter_2/Write Up/Redraft/Figures")
 
-p4 <- ggplot(testalpha, aes(x=value, fill=group)) + geom_density(alpha=.5)+
-  scale_x_continuous(expand = c(0, 0), name = expression(paste("Transmission-related Antibiotic Resistant Fitness Cost (", alpha, ")"))) + 
-  scale_y_continuous(limits = c(0,12),expand = c(0, 0), name = "") +
-  labs(fill = NULL) + scale_fill_discrete(labels = c("Generation 1", "Generation 2", "Generation 3", "Generation 4", "Generation 5")) +
-  theme(legend.text=element_text(size=14),  axis.text=element_text(size=14),
-        axis.title.y=element_text(size=14),axis.title.x= element_text(size=14), plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
 
 # Model Fit with Data -----------------------------------------------------
 
@@ -141,25 +164,52 @@ colnames(icombhdata)[1:8] <- c("tau", "SuscHumans","InfHumans","ResInfHumans","I
 icombhdata[,2:5] <- icombhdata[,2:5]*100000 #Scaling the prevalence (per 100,000)
 
 amp <- ggplot(dataamp, aes(x = pig_amp_sales/1000, y= ResPropAnim))  + geom_point() + theme_bw() + 
-  scale_x_continuous(expand = c(0, 0), limits = c(0,0.035)) + scale_y_continuous(expand = c(0, 0), limits = c(0,1)) +
-  labs(x ="Livestock Antibiotic Usage (g/PCU)", y = "Antibiotic-Resistant Livestock Carriage") +
+  scale_x_continuous(expand = c(0, 0), limits = c(0, 0.031)) + scale_y_continuous(expand = c(0, 0), limits = c(0,1)) +
+  labs(x ="Fattening Pig Ampicillin Sales (g/PCU)", y = "Ampicillin-Resistant Fattening Pig Carriage") +
   geom_errorbar(aes(ymin=lower, ymax=upper), colour="black", size=1.01, inherit.aes =  TRUE) + 
-  geom_line(data = icombhdata[icombhdata$group == "MAPtet",], aes(x = tau, y= IResRatA), col = "red", size = 1.02)
+  geom_line(data = icombhdata[icombhdata$group == "MAPtet",], aes(x = tau, y= IResRatA), col = "red", size = 1.02) +
+  theme(legend.text=element_text(size=12), axis.text=element_text(size=12), 
+        axis.title.y=element_text(size=12), axis.title.x= element_text(size=12), plot.margin = unit(c(1,1,1,1), "cm"))
 
 tet <- ggplot(datatetra, aes(x = pig_tetra_sales/1000, y= ResPropAnim))  + geom_point() + theme_bw() + 
-  scale_x_continuous(expand = c(0, 0), limits = c(0,0.035)) + scale_y_continuous(expand = c(0, 0), limits = c(0,1)) +
-  labs(x ="Livestock Antibiotic Usage (g/PCU)", y = "Antibiotic-Resistant Livestock Carriage") +
+  scale_x_continuous(expand = c(0, 0), limits = c(0,0.033)) + scale_y_continuous(expand = c(0, 0), limits = c(0,1)) +
+  labs(x ="Fattening Pig Tetracycline Sales (g/PCU)", y = "Tetracycline-Resistant Fattening Pig Carriage") +
   geom_errorbar(aes(ymin=lower, ymax=upper), colour="black", size=1.01, inherit.aes =  TRUE) + 
-  geom_line(data = icombhdata[icombhdata$group == "MAPamp",], aes(x = tau, y= IResRatA), col = "red", size = 1.02)
+  geom_line(data = icombhdata[icombhdata$group == "MAPamp",], aes(x = tau, y= IResRatA), col = "red", size = 1.02)+
+  theme(legend.text=element_text(size=12), axis.text=element_text(size=12), 
+        axis.title.y=element_text(size=12), axis.title.x= element_text(size=12), plot.margin = unit(c(1,1,1,1), "cm"))
 
 broil <- ggplot(databroil, aes(x = tetra_sales/1000, y= ResPropAnim))  + geom_point() + theme_bw() + 
-  scale_x_continuous(expand = c(0, 0), limits = c(0,0.035)) + scale_y_continuous(expand = c(0, 0), limits = c(0,1)) +
-  labs(x ="Livestock Antibiotic Usage (g/PCU)", y = "Antibiotic-Resistant Livestock Carriage") +
+  scale_x_continuous(expand = c(0, 0), limits = c(0,0.022)) + scale_y_continuous(expand = c(0, 0), limits = c(0,1)) +
+  labs(x ="Broiler Poultry Tetracycline Sales (g/PCU)", y = "Tetracycline-Resistant Broiler Poultry Carriage") +
   geom_errorbar(aes(ymin=lower, ymax=upper), colour="black", size=1.01, inherit.aes =  TRUE) + 
-  geom_line(data = icombhdata[icombhdata$group == "MAPbroil",], aes(x = tau, y= IResRatA), col = "red", size = 1.02)
+  geom_line(data = icombhdata[icombhdata$group == "MAPbroil",], aes(x = tau, y= IResRatA), col = "red", size = 1.02)+
+  theme(legend.text=element_text(size=12), axis.text=element_text(size=12), 
+        axis.title.y=element_text(size=12), axis.title.x= element_text(size=12), plot.margin = unit(c(1,1,1,1), "cm"))
 
-ggarrange(amp, tet, broil, nrow = 1, ncol = 3) 
+
+fits <- ggarrange(amp, tet, broil, nrow = 1, ncol = 3, align = "h", labels = c("A","B", "C"), font.label = c(size = 20),
+                  common.legend = TRUE, legend = "bottom") 
+
+ggsave(fits, filename = "Model_Fits.png", dpi = 300, type = "cairo", width = 14, height = 5, units = "in",
+       path = "C:/Users/amorg/Documents/PhD/Chapter_2/Write Up/Redraft/Figures")
+
 
 # Tau and ICombH ----------------------------------------------------------
 
+plotdata <- melt(icombhdata[icombhdata$group == "MAPtet",],
+                 id.vars = c("tau"), measure.vars = c("ResInfHumans","InfHumans")) 
+
+ggplot(plotdata, aes(fill = variable,x = tau, y = value))+ theme_bw() + 
+  geom_bar(position="stack", stat="identity") + scale_x_discrete(expand = c(0, 0)) + 
+  scale_y_continuous(limits = c(0,5), expand = c(0, 0))  + 
+  geom_text(data=datatetra, aes(x=pig_tetra_sales/1000, y=Freq, label=lab),vjust=0)
+
+plot_ly(output2, x= ~tau, y = ~InfHumans, type = "bar", name = "Antibiotic-Sensitive Infection (Human)") %>%
+  add_trace(y= ~ResInfHumans, name = "Antibiotic-Resistant Infection (Human)", textfont = list(size = 30)) %>% 
+  layout(yaxis = list(title = "Proportion of Infected Humans (ICombH) per 100,000", range = c(0,6), showline = TRUE),
+         xaxis = list(title = "Livestock Antibiotic Usage (g/PCU)"),
+         legend = list(orientation = "v", x = 0.6, y=1), showlegend = T,
+         barmode = "stack", annotations = list(x = ~tau, y = ~ICombH, text = ~IResRat, yanchor = "bottom", showarrow = FALSE, textangle = 310,
+                                               xshift =3))
 
